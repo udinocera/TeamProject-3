8fad1a032c33df45ab921b88c7a74a20a5d665b2
Denis Bruno <73432844+owlpsycho@users.noreply.github.com>
2023-07-12 16:41:30 +0200

    Update team.js

--- team.js
+++ team.js
@@ -9,17 +9,4 @@ const arr = [{
     favoriteFilm:"Inception",
     favoriteBook:"Uno, Nessuno e Centomila",
     petName:" "
-    },
-    {
- name : "Umberto",
- surname : "Di Nocera",
- age : 21,
- city : "Castellammare Di Stabia",
- hobby : "Tennis",
- favoriteFood : "Pizza",
- favoriteVideoGame : "GTA V",
- favoriteFilm : "La vita è bella",
- favoriteBook :  "Steve Jobs",
- petName : "Baby",
-}];
-
+    }]
