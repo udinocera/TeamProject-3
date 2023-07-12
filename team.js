49d83e97d3f00c2b253fe18b05d19e085c6202bb
Denis Bruno <73432844+owlpsycho@users.noreply.github.com>
2023-07-12 17:50:41 +0200

    funzione nomi uguali

--- team.js
+++ team.js
@@ -1,12 +1,34 @@
-const arr = [{
-    name:"Denis",
-    surname:"Bruno",
-    age:21,
-    city:"Santa Croce Camerina",
-    hobby:"play videogame",
-    favoriteFood:"Pizza",
-    favoriteVideoGame:"Grand Theft Auto V",
-    favoriteFilm:"Inception",
-    favoriteBook:"Uno, Nessuno e Centomila",
-    petName:" "
-    }]
+function controllaNomi(arr) {
+    let names = {};
+    let duplicateName = [];
+  
+    for (i = 0; i < arr.length; i++) {
+      let name = arr[i].name;
+  
+      if (names[name]) {
+        var isDuplicate = false;
+        for (j = 0; j < duplicateName.length; j++) {
+          if (duplicateName[j] === name) {
+            isDuplicate = true;
+            break;
+          }
+        }
+  
+        if (!isDuplicate) {
+          duplicateName.push(name);
+        }
+      }
+  
+      names[name] = true;
+    }
+    return duplicateName;
+  }
+  
+  var duplicateName = controllaNomi(arr);
+  
+  if (duplicateName.length > 0) {
+    console.log("Ci sono membri con lo stesso nome nell'array.");
+    console.log("Nomi duplicati: " + duplicateName.join(", "));
+  } else {
+    console.log("Non ci sono membri con lo stesso nome nell'array.");
+  }
