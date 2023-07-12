5bb21561d28a1e78235a7a8c1b1a8f783bdd2770
udinocera <udinocera@gmail.com>
2023-07-12 17:59:00 +0200

    Funzione fino a Denis

--- team.js
+++ team.js
@@ -54,3 +54,40 @@ function printPetName(petName){
 }
 
 printPetName(arr)
+
+
+
+function controllaNomi(arr) {
+    let names = {};
+    let duplicateName = [];
+
+    for (i = 0; i < arr.length; i++) {
+      let name = arr[i].name;
+
+      if (names[name]) {
+        var isDuplicate = false;
+        for (j = 0; j < duplicateName.length; j++) {
+          if (duplicateName[j] === name) {
+            isDuplicate = true;
+            break;
+          }
+        }
+
+        if (!isDuplicate) {
+          duplicateName.push(name);
+        }
+      }
+
+      names[name] = true;
+    }
+    return duplicateName;
+  }
+
+  var duplicateName = controllaNomi(arr);
+
+  if (duplicateName.length > 0) {
+    console.log("Ci sono membri con lo stesso nome nell'array.");
+    console.log("Nomi duplicati: " + duplicateName.join(", "));
+  } else {
+    console.log("Non ci sono membri con lo stesso nome nell'array.");
+  }
