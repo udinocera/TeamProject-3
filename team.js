f3b426a33d4045ddee1dcd5faa5cb96bec9d21c0
udinocera <udinocera@gmail.com>
2023-07-12 16:30:02 +0200

    Scelte di Umberto

--- team.js
+++ team.js
@@ -1,1 +1,13 @@
-const arr = [];
+const arr = [
+    {
+ name : "Umberto",
+ surname : "Di Nocera",
+ age : 21,
+ city : "Castellammare Di Stabia",
+ hobby : "Tennis",
+ favoriteFood : "Pizza",
+ favoriteVideoGame : "GTA V",
+ favoriteFilm : "La vita è bella",
+ favoriteBook :  "Steve Jobs",
+ petName : "Baby",
+}];
