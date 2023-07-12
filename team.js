3c4d553a59a571428380bca27f485bc5fdc2519a
Denis Bruno <denisbruno.DB28@gmail.com>
2023-07-12 16:22:52 +0200

    completamento file (denis)

--- team.js
+++ team.js
@@ -1,1 +1,12 @@
-const arr = [];
+const arr = [{
+    name:"Denis",
+    surname:"Bruno",
+    age:21,
+    city:"Santa Croce Camerina",
+    hobby:"play videogame",
+    favoriteFood:"Pizza",
+    favoriteVideoGame:"Grand Theft Auto V",
+    favoriteFilm:"Inception",
+    favoriteBook:"Uno, Nessuno e Centomila",
+    petName:" "
+}]
