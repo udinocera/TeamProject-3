3f6ebf00f153df099d13d5961e56a8d707135079
marco <marco2000bruno@gmail.com>
2023-07-12 16:22:33 +0200

    update marco

--- team.js
+++ team.js
@@ -1,1 +1,12 @@
-const arr = [];
+const arr = [{
+    name:"marco",
+    surname:"bruno",
+    age: 23 ,
+    city: "napoli",
+    hobby: "gym",
+   favotiteFood: "pizza",
+   favoriteVideoGames:" fifa",
+   favoriteBook: "harry potter",
+   favoriteFilm: "inception",
+   petName: ""
+}];
