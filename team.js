f58a7f02ef856ad2940b8358fd27d832edfaa362
Massimo <rabuffo.massimo@gmail.com>
2023-07-12 16:25:09 +0200

    commit by massimo

--- team.js
+++ team.js
@@ -1,1 +1,12 @@
-const arr = [];
+const arr = [{
+    name: "Massimo",
+    surname: "Rabuffo",
+    age: "26",
+    city:"Caserta",
+    hobby: "web developer",
+    favouriteFood: "Pizza",
+    favouriteVideogame: "The last of us",
+    favouriteFilm:"interstellar",
+    favouriteBook:"La strada",
+    petName:"Chloe",
+}];
